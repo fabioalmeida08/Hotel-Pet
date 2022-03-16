60179853edaca17b21f7192986a967d336f70b89
Fábio <fabioalmeida0896@gmail.com>
2022-03-16 14:52:56 -0300

    css : reset css

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Router from './routes'
+import {CssBaseline} from '@mui/material'
 
 function App() {
   return (
     <>
+      <CssBaseline/>
       <Router />
       <ToastContainer />
     </>
