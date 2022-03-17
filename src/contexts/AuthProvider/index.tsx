9e25848cb23692b40a76449fea76262b6f2eb500
Fábio <fabioalmeida0896@gmail.com>
2022-03-17 19:11:40 -0300

    feat: added Authentication context

---
+++ src/contexts/AuthProvider/index.tsx
@@ -0,0 +1,68 @@
+import {
+  createContext,
+  ReactNode,
+  useContext,
+  useState,
+} from 'react'
+import { useNavigate } from 'react-router-dom'
+import hotelPetApi from '../../services/index'
+
+interface AuthProviderProps {
+  children: ReactNode
+}
+
+interface IFormData {
+  email: string
+  password: string
+}
+
+interface AuthProviderValue {
+  signIn: (userData: IFormData) => void
+  authToken: string
+  logOut: () => void
+}
+
+const AuthContext = createContext<AuthProviderValue>(
+  {} as AuthProviderValue
+)
+
+const useAuth = () => useContext(AuthContext)
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const navigate = useNavigate()
+
+  const [authToken, setAuthToken] = useState(
+    () => localStorage.getItem('@hotelPet:token') || ''
+  )
+
+  const signIn = (userData: IFormData) => {
+    hotelPetApi
+      .post('/login', userData)
+      .then((res) => {
+        const id = res.data.user.id
+        localStorage.setItem(
+          '@hotelPet:token',
+          JSON.stringify(res.data.accessToken)
+        )
+        setAuthToken(res.data)
+        navigate(`/dashboard/${id}`)
+      })
+      .catch((err) => console.log(err))
+  }
+
+  const logOut = () => {
+    localStorage.clear()
+    setAuthToken('')
+    navigate('/')
+  }
+
+  return (
+    <AuthContext.Provider
+      value={{ signIn, authToken, logOut }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export { AuthProvider, useAuth }
