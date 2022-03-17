065c6e82a89b48ae9736f2bd801a2d63922922ce
assemble05 <joneto18.jh@gmail.com>
2022-03-17 19:23:50 -0300

    feat: CardRegisterPet

--- src/components/cardRegisterPet/index.tsx
+++ src/components/cardRegisterPet/index.tsx
@@ -1,164 +1,190 @@
-import {InputLabel, MenuItem, Select, TextField } from '@mui/material';
-import FormControl from '@mui/material/FormControl';
-import { StyledForm, DivForm, DivWrapper, DivHeader } from './styles';
-import pata from "./images/pata.png"
+import { MenuItem, TextField } from '@mui/material';
+import { StyledForm, DivButtons, DivWrapper, DivHeader, DivImgDesktop } from './styles';
+import DogPaw from "./images/pata.png"
 import backgroundCat from "./images/backgroundRegisterCat.png"
 import * as yup from 'yup';
 import { Controller, useForm } from "react-hook-form"
 import { yupResolver } from '@hookform/resolvers/yup';
 import axios from 'axios';
-import { useState } from 'react';
 
 
 
 const CardRegisterPet = () => {
 
-    interface IFormData {
-    name: string
-    email: string
-    password: string
-    passwordConfirm: string
-  }
-  
-    const schema = yup.object().shape({
-       name: yup.string().required(),
-        specie: yup.string().required(),
-        race: yup.string().required(),
-        age: yup.number().required(),
-        size: yup.string().required()
-    })
-    const {
-      handleSubmit,
-      control,
-      formState: { errors },
-    } = useForm({
-      resolver: yupResolver(schema),
-    })
-  
-    const token = "token"
+  const schema = yup.object().shape({
+    name: yup.string().required(),
+    specie: yup.string().required(),
+    race: yup.string().required(),
+    age: yup.number().required(),
+    size: yup.string().required()
+  })
 
-    const onSubmit = handleSubmit((data) => {
-      
-        axios.post("https://hotelpetapi.herokuapp.com/pets", data, {
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
 
-        }).then((response) => console.log(response)).catch((err) => console.log(err))
-    })
+  const onSubmit = handleSubmit((data) => {
 
-    return (
-        <DivWrapper>
-            <div>
-                <img src={backgroundCat} alt="backgroundCat"></img>
-            </div>
-            <DivForm>
-                <DivHeader>
+    // axios.post("https://hotelpetapi.herokuapp.com/pets", data, {
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
 
-                    <h1>Cadastro do Pet</h1>
-                    <img src={pata} alt="pata"></img>
+    // }).then((response) => console.log(response)).catch((err) => console.log(err))
+    
+  })
 
-                </DivHeader>
-                <StyledForm onSubmit={onSubmit}>
-                <Controller
-            name='name'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                {...field}
-                required
-                fullWidth
-                margin='normal'
-                id='name'
-                label='Nome do Pet'
-                autoFocus
-                helperText={errors.name?.message}
-                error={!!errors.name?.message}
-              />
-            )}
-          />
-          <Controller
-            name='specie'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                {...field}
-                required
-                fullWidth
-                margin='normal'
-                id='specie'
-                label='Espécie'
-                helperText={errors.specie?.message}
-                error={!!errors.specie?.message}
-              />
-            )}
-          />
-          <Controller
-            name='race'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                {...field}
-                required
-                fullWidth
-                margin='normal'
-                id='race'
-                label='Raça'
-                type='text'
-                helperText={errors.race?.message}
-                error={!!errors.race?.message}
-              />
-            )}
-          />
-          <Controller
-            name='age'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                {...field}
-                required
-                fullWidth
-                margin='normal'
-                id='age'
-                label='Raça'
-                type='age'
-                helperText={errors.passwordConfirm?.message}
-                error={!!errors.passwordConfirm?.message}
-              /> )} />
-            <Controller
-            name='size'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                {...field}
-                required
-                fullWidth
-                margin='normal'
-                id='size'
-                label='Porte'
-                type='size'
-                helperText={errors.passwordConfirm?.message}
-                error={!!errors.passwordConfirm?.message}
-              /> )} />
-         
-              
+  return (
+    <DivWrapper>
+
+      <DivImgDesktop>
+        <img src={backgroundCat} alt="backgroundCat"></img>
+      </DivImgDesktop>
+
+      <StyledForm onSubmit={onSubmit}>
+
+        <DivHeader>
+          <h1>Cadastro do Pet</h1>
+          <span></span>
+        
+          <img src={DogPaw} alt="Pata de Cachorro"></img>
+        </DivHeader>
+
+        <Controller
+          name='name'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              fullWidth
+              label="Nome do Pet"
+              margin='none'
+              id="outlined-size-small"
+              size="small"
+              autoFocus
+              helperText={errors.name?.message}
+              error={!!errors.name?.message}
+            />
+          )}
+        />
+        <Controller
+          name='race'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              fullWidth
+              margin='normal'
+              id='race'
+              label='Raça'
+              type='text'
+              size="small"
+              helperText={errors.race?.message}
+              error={!!errors.race?.message}
+            />
+          )}
+        />
+        <Controller
+          name='age'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              {...field}
+              required
+              fullWidth
+              margin='normal'
+              id='age'
+              size="small"
+              label='Idade'
+              type='age'
+              helperText={errors.passwordConfirm?.message}
+              error={!!errors.passwordConfirm?.message}
+            />)} />
+        <Controller
+          name='specie'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              label="Espécie"
+              select
+              margin="normal"
+              required
+              size="small"
+              defaultValue=""
+              {...field}
+              helperText={errors.specie?.message}
+              error={!!errors.specie?.message}
+              sx={{ textOverflow: 'ellipsis', width: '100%' }}
+            >
+              <MenuItem value={'Cachorro'}>
+                Cachorro
+              </MenuItem>
+              <MenuItem value={'Gato'}>
+                Gato
+              </MenuItem>
+              <MenuItem value={'Aves'}>
+                Aves
+              </MenuItem>
+              <MenuItem value={'Reptil'}>
+                Reptil
+              </MenuItem>
+              <MenuItem value={'Alien'}>
+                Alien
+              </MenuItem>
+              <MenuItem value={'Pré Histórico'}>
+                Pré Historico
+              </MenuItem>
+            </TextField>
+          )} />
+        <Controller
+          name='size'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              label="Porte"
+              select
+              margin="normal"
+              required
+              size="small"
+              defaultValue=""
+              {...field}
+              helperText={errors.size?.message}
+              error={!!errors.size?.message}
+              sx={{ textOverflow: 'ellipsis', width: '100%' }}
+            >
+              <MenuItem value={'Pequeno'}>
+                Pequeno
+              </MenuItem>
+              <MenuItem value={'Médio'}>
+                Médio
+              </MenuItem>
+              <MenuItem value={'Grande'}>
+                Grande
+              </MenuItem>
+
+            </TextField>
+          )} />
 
 
+        <DivButtons>
 
+          <button type="submit">Cadastrar</button>
+          <button className='backButton' type="submit">Voltar</button>
+        </DivButtons>
 
-
-                    <button type="submit">Cadastrar</button>
-
-                </StyledForm>
-            </DivForm>
-
-
-        </DivWrapper>
-    )
+      </StyledForm>
+    </DivWrapper>
+  )
 }
 export default CardRegisterPet
--- src/components/cardRegisterPet/styles.ts
+++ src/components/cardRegisterPet/styles.ts
@@ -1,46 +1,77 @@
 import styled from "styled-components";
 
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  height: 100vw;
-  max-height: 340px;
-  width: 45vw;
-  max-width: 200px;
-`
 const DivWrapper = styled.div`
+  box-shadow: 1px 1px 1px 1px gray;
   display: flex;
-  flex-direction: "row";
-  height: 317px;
-  width: 308px;
-  border-radius: 15.8px;
-  background-color: #fff;
-  img {
-    height: 100vw;
-    max-height: 350px;
-    width: 38vw;
-    max-width: 150px;
+  justify-content: center;
+  width: 280px;
+  height: 440px;
+  border-radius: 8px;
+
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    padding: 0px 50px 0px 0px;
+    width: 400px;
+    height: 436px;
   }
 `;
+
+const StyledForm = styled.form`
+  padding: 0px 30px;
+  @media (min-width: 768px) {
+    padding: 0px 0px 0px 10px;
+  }
+`;
+
 const DivHeader = styled.div`
   display: flex;
   flex-direction: "row";
   align-items: center;
   justify-content: space-around;
+  padding-right: 20px;
   width: 180px;
   h1 {
     font-size: 16px;
+    color: #333333;
+    text-decoration: underline #f4a91999 3px;
   }
   img {
     width: 30px;
     height: 30px;
   }
 `;
-const DivForm = styled.div`
-  height: 80vw;
-  max-height: 350px;
-  width: 58vw;
-  max-width: 150px;
-  padding-left: 10px;
+const DivImgDesktop = styled.div`
+  display: none;
+  @media (min-width: 768px) {
+    display: block;
+    img {
+      height: 437.5px;
+      width: 169px;
+    }
+  }
 `;
-export { StyledForm, DivForm, DivWrapper, DivHeader };
+const DivButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-left: 12px;
+  width: 192px;
+
+  button {
+    width: 170px;
+    height: 35px;
+    border-radius: 5px;
+    border-style: none;
+    color: #ffff;
+    margin-top: 8px;
+    background-color: #354fd8;
+  }
+  .backButton {
+    background-color: orange;
+  }
+  @media (min-width: 768px) {
+    margin-left: 1px;
+  }
+`;
+export { StyledForm, DivButtons, DivWrapper, DivHeader, DivImgDesktop };
