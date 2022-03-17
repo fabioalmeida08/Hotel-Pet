0e69846b464f572a0808b3ac6aca6d9ab02aba35
chris <cbcr93@gmail.com>
2022-03-17 09:59:25 -0300

    fix: route indentation and specification

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,10 @@ import Signup from "../pages/signup";
 const Router = () => {
   return (
     <Routes>
-       <Route path="/" element={<Home />}>
+      <Route path="/" element={<Home />}>
         <Route path="login" element={<Login />}/>
         <Route path="signup" element={<Signup />}/>
-        <Route path="dashboard" element={<Dashboard />}/>
+        <Route path="dashboard/:id" element={<Dashboard />}/>
       </Route>
     </Routes>
   )
