554b4fce19e4debc2bc0275bcbe539dae0efdca6
assemble05 <joneto18.jh@gmail.com>
2022-03-18 19:17:17 -0300

    refactor: image path

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Router from './routes'
 import {CssBaseline} from '@mui/material'
-import CardRegisterPet from './components/cardRegisterPet'
 function App() {
   return (
     <>
       <CssBaseline/>
       <Router />
-      <CardRegisterPet></CardRegisterPet>
       <ToastContainer />
     </>
   )
