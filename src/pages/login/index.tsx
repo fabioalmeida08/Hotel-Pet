8b6865f22df82edf1a30d65502af795f585a0adb
Fábio <fabioalmeida0896@gmail.com>
2022-03-19 09:19:09 -0300

    fix: login route fixed

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,89 @@
+import {
+  Container,
+  Stack,
+  Typography,
+  Paper,
+  Grid,
+} from '@mui/material'
+import useMediaQuery from '@mui/material/useMediaQuery'
 import LoginForm from '../../components/LoginForm'
-const Login = () => {
+import Logo from '../../assets/svg/logo.svg'
+import Cachorro from '../../assets/svg/cachorroLogo.svg'
+const Signup = () => {
+  const matches = useMediaQuery('(max-width:1110px)')
+
   return (
     <>
-      <LoginForm />
+      {matches ? (
+        <Container
+          maxWidth='xs'
+          sx={{
+            mt: '15vh',
+            img: { width: '80px', alignSelf: 'start' },
+          }}
+        >
+          <Stack sx={{}}>
+            <img src={Logo} alt='logo' />
+            <Typography variant='h3' alignSelf='center'>
+              Login
+            </Typography>
+            <LoginForm />
+          </Stack>
+        </Container>
+      ) : (
+        <Container
+          maxWidth='xl'
+          sx={{
+            mt: '1vh',
+            img: { height: 'fitContainer' },
+          }}
+        >
+          <Grid container justifyContent='center' >
+            <Grid
+              item
+              sx={{
+                img: { width: '100px', alignSelf: 'start' },
+                bgcolor: '#EFEFEF',
+                borderTopLeftRadius: '20px',
+                borderBottomLeftRadius: '20px',
+                boxShadow: '10px 10px 5px 0px rgba(0,0,0,0.21)'
+              }}
+            >
+              <Stack sx={{ mt: '2vh' }}>
+                <img src={Logo} alt='logo' />
+                <Typography
+                  variant='h3'
+                  alignSelf='end'
+                  mr='30px'
+                >
+                  Login
+                </Typography>
+                <LoginForm />
+              </Stack>
+            </Grid>
+            <Grid
+              item
+              sx={{
+                borderTopRightRadius: '20px',
+                borderBottomRightRadius: '20px',
+                height: 'fitContent',
+                bgcolor: '#EFEFEF',
+                img:{
+                  height: 'fitContainer'
+                },
+                boxShadow: '10px 10px 5px 0px rgba(0,0,0,0.21)'
+              }}
+            >
+              <img
+                src={Cachorro}
+                alt='cachorro segurando uma rosa'
+              />
+            </Grid>
+          </Grid>
+        </Container>
+      )}
     </>
   )
 }
 
-export default Login
+export default Signup
