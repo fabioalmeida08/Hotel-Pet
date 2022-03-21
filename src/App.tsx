136a0fe3be2231fa1a01518b17d628b9cd27575d
igorcenzi <igor.cenzi2@gmail.com>
2022-03-21 19:57:00 -0300

    feat: cardPetStatus backend working

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Router from './routes'
 import {CssBaseline} from '@mui/material'
+import CardPetsStatus from './components/CardPetsStatus'
 
 function App() {
   return (
     <>
       <CssBaseline/>
+      <CardPetsStatus idPet={'13'}/>
       <Router />
       <ToastContainer />
     </>
