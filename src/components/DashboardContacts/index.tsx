ccfbd9c4732c86bcd81831e8ea3022480fd70bb1
chris <cbcr93@gmail.com>
2022-03-21 20:11:21 -0300

    feaat: base contacts component and styles

--- src/components/DashboardContacts/index.tsx
+++ src/components/DashboardContacts/index.tsx
@@ -1,10 +1,29 @@
-import Container from "./stylesDashboardContacts"
-
+import { Container, ContainerInfo, ContainerHotel, ContainerAll, ContainerChat }  from "./stylesDashboardContacts"
+import map from "../../assets/svg/map.svg"
 const DashboardContacts = () => {
-    return (
-      <Container>
-
-      </Container>
+    return ( 
+      <ContainerAll>
+          <h2>Contatos</h2>
+        <Container>
+          <ContainerHotel>
+            <ContainerInfo>
+              <h3>Telefone:</h3>
+              <p>(00) 0000-0000</p>
+            </ContainerInfo>
+            <ContainerInfo>
+              <h3>Whatsapp:</h3>
+              <p>(00) 99999-9999</p>
+            </ContainerInfo>
+            <ContainerInfo>
+              <h3>Endereço:</h3>
+              <p>Av. Parana, do outro lado de lá, nº1</p>
+            </ContainerInfo>
+            <img src={map} alt="" />
+            
+          </ContainerHotel>
+          <ContainerChat></ContainerChat>
+        </Container>
+      </ContainerAll>
     )
   }
   
