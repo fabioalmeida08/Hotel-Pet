f85b4d67cf72e52de70b8b846f301632cc0ca80e
Fábio <fabioalmeida0896@gmail.com>
2022-03-21 10:21:06 -0300

    feat: dashboard route protected

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,14 +1,21 @@
-import DasboardHeader from "../../components/HeaderDashboard"
-import CardUser from "../../components/CardUser"
-import { Container } from "./styles"
-
+import DasboardHeader from '../../components/HeaderDashboard'
+import CardUser from '../../components/CardUser'
+import { Container } from './styles'
+import { useAuth } from '../../contexts/AuthProvider'
+import { Navigate } from 'react-router-dom'
 const Dashboard = () => {
-    return (
-      <Container>
-        <DasboardHeader />
-        <CardUser />
-      </Container>
-    )   
+  const {authToken} = useAuth()
+  
+  if (!authToken) {
+    return <Navigate to='/login' replace />
   }
   
-  export default Dashboard
+  return (
+    <Container>
+      <DasboardHeader />
+      <CardUser />
+    </Container>
+  )
+}
+
+export default Dashboard
