c26e568852ad0e8c463b2e794cb9e7bba51b32c1
assemble05 <joneto18.jh@gmail.com>
2022-03-22 13:30:49 -0300

    fix : App rendering component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Router from './routes'
 import {CssBaseline} from '@mui/material'
-import CardMimos from './components/CardMimos'
 function App() {
   return (
     <>
       <CssBaseline/>
       <Router />
-      <CardMimos img="asd" title='sadsad' description='dshudh sadhu ch jhxcxc x'/>
       <ToastContainer />
     </>
   )
