3b004f1e6d0a652cdeaa005bff488f08949b4003
assemble05 <joneto18.jh@gmail.com>
2022-03-22 08:48:58 -0300

    feat: CardMimos

---
+++ src/components/CardMimos/index.tsx
@@ -0,0 +1,102 @@
+import { useState } from "react"
+import { StyledDivMimos, CardBuyMime } from "./style"
+import Cat from "../../assets/img/CatMime.png"
+import { Controller } from "react-hook-form"
+import { MenuItem, TextField } from "@mui/material"
+import * as yup from 'yup';
+import { useForm } from "react-hook-form"
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useAuth } from "../../contexts/AuthProvider"
+import axios from "axios"
+interface CardTypes {
+  img: string,
+  title: string,
+  description: string
+}
+interface PetsType {
+  age: number
+  hospedado: boolean
+  id: number
+  mimos: Array<object>
+  name: string
+  race: string
+  size: string
+  specie: string
+  status: Array<object>
+  tutorId: number
+}
+const CardMimos = ({ img, title, description }: CardTypes) => {
+  const [isBuying, setIsBuying] = useState(false)
+  const [userPets, setUserPets] = useState<PetsType[]>([])
+  const [filteredPets, setFilteredPets] = useState<PetsType[]>([])
+  const { userId } = useAuth()
+
+  const schema = yup.object().shape({
+    petBuyMime: yup.string().required(),
+  })
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  })
+  const onSubmit = handleSubmit((data) => {
+    
+  })
+
+  return (
+    <>
+      {isBuying === true ? <CardBuyMime>
+        <img src={Cat} alt="CatImage"></img>
+        <div className="CardContainer">
+          <h2>Comprar o {title}</h2>
+          <Controller
+          name='petBuyMime'
+          control={control}
+          defaultValue=''
+          render={({ field }) => (
+            <TextField
+              label="Nome do Pet"
+              select
+              margin="normal"
+              required
+              size="small"
+              defaultValue=""
+              {...field}
+              helperText={errors.petBuyMime?.message}
+              error={!!errors.petBuyMime?.message}
+              sx={{ textOverflow: 'ellipsis', width: '100%' }}
+            >
+              {filteredPets.map((pet) => {return <MenuItem value={pet.name}>
+              {pet.name}
+              </MenuItem>})}
+            </TextField>
+          )} />
+          <p>Confirmar compra do produto</p>
+        </div>
+        <button onClick={() => setIsBuying(!isBuying)}>
+          Comprar
+        </button>
+
+      </CardBuyMime> : <StyledDivMimos>
+        <img src={img} alt={title}></img>
+        <div className="CardContainer">
+
+          <h2>{title}</h2>
+          <p>
+            {description}
+          </p>
+          <button onClick={() => {
+
+            setIsBuying(!isBuying)
+            }}>
+            Comprar
+          </button>
+        </div>
+      </StyledDivMimos>}
+    </>
+  )
+}
+export default CardMimos
---
+++ src/components/CardMimos/style.ts
@@ -0,0 +1,102 @@
+import styled from "styled-components";
+
+const StyledDivMimos = styled.div`
+  width: 290px;
+  height: 180px;
+  display: flex;
+  align-items: flex-start;
+  border-radius: 20px;
+  background-color: #ffffff;
+  color: #333333;
+  font-size: 14px;
+  justify-content: space-evenly;
+  box-shadow: 1px 1px 1px 1px gray;
+  position: relative;
+  @media (min-width: 768px) {
+    width: 350px;
+  }
+  img {
+    height: 100%;
+    width: 40%;
+    border-radius: 20px 0 0 20px;
+    flex: 1;
+    background-size: cover;
+    background-position: center;
+    object-fit: cover;
+  }
+  .CardContainer {
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    padding: 15px 10px 15px 8px;
+    position: relative;
+
+    button {
+      width: 140px;
+      position: absolute;
+      top: 130px;
+      left: 10px;
+    }
+  }
+
+  button {
+    padding: 8px 16px;
+    border: none;
+    border-radius: 4px;
+    background: linear-gradient(
+      180deg,
+      rgba(244, 169, 25, 0.52) 0%,
+      #f4a919 100%
+    );
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: "Roboto";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+    margin-top: 8px;
+  }
+`;
+const CardBuyMime = styled.div`
+  width: 250px;
+  height: 400px;
+  display: flex;
+  border-radius: 11px;
+  background-color: #ffffff;
+  color: #333333;
+  font-size: 14px;
+  box-shadow: 1px 1px 1px 1px gray;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+
+  button {
+    padding: 8px 16px;
+    border: none;
+    border-radius: 4px;
+    background: linear-gradient(
+      180deg,
+      rgba(244, 169, 25, 0.52) 0%,
+      #f4a919 100%
+    );
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 500;
+    font-family: "Roboto";
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 4px;
+    margin-top: 8px;
+  }
+
+  img {
+    width: 250px;
+    height: 200px;
+    margin-top: 0px;
+  }
+`;
+
+export { StyledDivMimos, CardBuyMime };
