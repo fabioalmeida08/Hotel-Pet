6e59074258090596c303e9a8ae848c9d7ed2589e
assemble05 <joneto18.jh@gmail.com>
2022-03-22 15:44:46 -0300

    feature: dashboardMime inProgress

--- src/components/CardMimos/index.tsx
+++ src/components/CardMimos/index.tsx
@@ -1,137 +1,31 @@
-import { useState } from "react"
-import { StyledDivMimos, CardBuyMime } from "./style"
-import SquareCat from "../../assets/img/SquareCat.png"
-import { Controller } from "react-hook-form"
-import { MenuItem, TextField } from "@mui/material"
-import * as yup from 'yup';
-import { useForm } from "react-hook-form"
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useAuth } from "../../contexts/AuthProvider"
-import axios from "axios"
-import { toast, ToastContainer } from "react-toastify"
+import { StyledDivMimos } from "./style"
 
 interface CardTypes {
   img: string,
   title: string,
   description: string
+  setModal: Function
+  service: Function
 }
 
-const CardMimos = ({ img, title, description }: CardTypes) => {
-  const [isBuying, setIsBuying] = useState(false)
-
-  const { userPets, authToken } = useAuth()
-
-  const schema = yup.object().shape({
-    pet: yup.string().required(),
-  })
-
-  const {
-    handleSubmit,
-    control,
-    formState: { errors },
-  } = useForm({
-    resolver: yupResolver(schema),
-  })
-  const onSubmit = handleSubmit((data) => {
-    data.service = title
-    data.done = false
-    const petMime = userPets.filter((pet) => pet.name === data.pet)
-    const petId = petMime[0].id
-    const petToUse = petMime[0]
-    axios.put(`https://hotelpetapi.herokuapp.com/pets/${petId}`, {
-
-      "size": petToUse.size,
-      "age": petToUse.age,
-      "race": petToUse.race,
-      "specie": petToUse.specie,
-      "name": petToUse.name,
-      "hospedado": petToUse.hospedado,
-      "status": petToUse.status,
-      "mimos": [data],
-      "userId": petToUse.userId,
-      "id": petToUse.id
-    }, {
-      headers: {
-        Authorization: `Bearer ${authToken}`,
-      },
-
-    }).then((response) => {
-      if (response.status === 200) {
-
-        toast.success('Compra realizada com sucesso', {
-          position: "top-right",
-          autoClose: 3000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        })
-
-      }
-    })
-      .catch((err) => console.log(err.message))
-
-  })
-
+const CardMimos = ({ img, title, description, setModal, service }: CardTypes) => {
   return (
-    <>
-      {isBuying === true ? <CardBuyMime>
-        <ToastContainer />
-        <img src={SquareCat} alt="CatImage"></img>
-        <div className="DivCard">
-          <h2>Comprar o {title}</h2>
-          <Controller
-            name='pet'
-            control={control}
-            defaultValue=''
-            render={({ field }) => (
-              <TextField
-                label="Nome do Pet"
-                select
-                margin="normal"
-                required
-                size="small"
-                defaultValue=""
-                {...field}
-                helperText={errors.petBuyMime?.message}
-                error={!!errors.petBuyMime?.message}
-                sx={{ textOverflow: 'ellipsis', width: '100%' }}
-              >
-                {userPets.map((pet) => {
-                  return <MenuItem value={pet.name}>
-                    {pet.name}
-                  </MenuItem>
-                })}
 
-              </TextField>
-            )} />
-          <p>Confirmar compra do produto</p>
-        </div>
-        <button onClick={() => {
-          setIsBuying(!isBuying)
-          onSubmit()
-        }}>
+    <StyledDivMimos>
+      <img src={img} alt={title}></img>
+      <div className="CardContainer">
+
+        <h2>{title}</h2>
+        <p>
+          {description}
+        </p>
+        <button onClick={() => { setModal(true);service(title) }
+        }>
           Comprar
         </button>
+      </div>
+    </StyledDivMimos>
 
-      </CardBuyMime> : <StyledDivMimos>
-        <img src={img} alt={title}></img>
-        <div className="CardContainer">
-
-          <h2>{title}</h2>
-          <p>
-            {description}
-          </p>
-          <button onClick={() => {
-
-            setIsBuying(!isBuying)
-          }}>
-            Comprar
-          </button>
-        </div>
-      </StyledDivMimos>}
-    </>
   )
 }
 export default CardMimos
--- src/components/DashboardMime/index.tsx
+++ src/components/DashboardMime/index.tsx
@@ -1,10 +1,28 @@
-/* import Container from "./stylesDashboardMime" */
+import { useState } from "react"
+import CardMimos from "../CardMimos"
+import ConfirmBuy from "../ConfirmBuy"
+import Container from "./stylesDashboardMime"
 
 const DashboardMime = () => {
+      const [ modal , setModal] = useState(false)
+      const [service , setServiceType] = useState('')
+   const alterateState = (param : boolean) => {
+     setModal(param)
+   }
+   const setServie = (serviceType : string) => {
+    setServiceType(serviceType)
+   }
     return (
       <>
-        {/* <Container>
-        </Container> */}
+        {modal ? <ConfirmBuy setModal={alterateState} service={service} /> :
+         <Container>
+          <div>
+            <h2 className="title">Mimos</h2>
+            <span></span>
+          </div>
+        <CardMimos title="Sortido" service={setServie} setModal={alterateState} description="Um novo brinquedo aleatorio para o seu pet se divertir e brincar" img="asd"/>
+          <CardMimos title="Spa"  service={setServie} setModal={alterateState}  description="Todos os cuidados que seu pet merece" img="asd"/>
+        </Container>}
       </>
     )
   }
