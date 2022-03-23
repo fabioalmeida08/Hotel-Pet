ddb74001fb5578fe97c6831c4b8d24838655b365
chris <cbcr93@gmail.com>
2022-03-23 12:22:43 -0300

    feat: control pets base

--- src/pages/PagPets/index.tsx
+++ src/pages/PagPets/index.tsx
@@ -1,15 +1,19 @@
+import AdmPets from "../../components/AdmPets"
 import DashboardPets from "../../components/DashboardPets"
 import DasboardHeader from "../../components/HeaderDashboard"
 import UserNav from "../../components/UserNav"
+import { useAuth } from "../../contexts/AuthProvider"
 import { Container, CardUser } from "./styles"
 
 const PagPets = () => {
+    const { admin } = useAuth()
     return (
       <Container>
         <DasboardHeader />
         <CardUser>
           <UserNav />
-          <DashboardPets />
+          {admin ? <AdmPets/>: <DashboardPets />}
+          
         </CardUser>
       </Container>
     )   
