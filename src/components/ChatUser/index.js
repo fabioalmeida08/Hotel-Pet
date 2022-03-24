e8976837aced9a080bd731da5d5329f4c5014a91
assemble05 <joneto18.jh@gmail.com>
2022-03-24 19:41:02 -0300

    feat : chat 70%

--- src/components/ChatUser/index.js
+++ src/components/ChatUser/index.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../contexts/AuthProvider";
-import { DivWrapper,DivChat } from "./style";
+import { DivWrapper,DivInput, DivChat } from "./style";
 import { socket } from "../../services/chat";
 
 const UserChat = () => {
@@ -8,37 +8,38 @@ const UserChat = () => {
   const [message, setMessage] = useState("");
   const { userId } = useAuth();
   const [receivedMessage, setReceivedMessage] = useState([]);
-  const obj  = {
-      from : '1',
-      to : "admin",
-      message : "salve quebrada"
-  }
+  const sendMessage = {
+    from: "1",
+    to: "admin",
+    message: message,
+  };
   useEffect(() => {
-    io.emit("join_room", '1');
+    io.emit("join_room", "1");
     io.on("message", (data) => {
-      console.log(data);
-      setReceivedMessage(prev => [...prev, data])
+
+      setReceivedMessage((prev) => [...prev, data]);
     });
   }, []);
 
   const handleSubmit = () => {
-    io.emit('message', obj)
 
+    io.emit("message", sendMessage);
 
   };
 
   return (
     <DivWrapper>
+        <div className="superDiv">
+        <h2>Chat</h2>
+        </div>
       <DivChat>
-        <ul>
-          {receivedMessage.map(message => <li>{message.message}</li>)}
-        </ul>
+        <ul>{receivedMessage.map((per) => console.log(per))}</ul>
       </DivChat>
 
-      <div>
+      <DivInput>
         <input onChange={(e) => setMessage(e.target.value)} />
         <button onClick={() => handleSubmit()}>Enviar mensagem</button>
-      </div>
+      </DivInput>
     </DivWrapper>
   );
 };
--- src/components/ChatUser/style.ts
+++ src/components/ChatUser/style.ts
@@ -1,13 +1,66 @@
 import styled from "styled-components";
 
 const DivWrapper = styled.div`
-   
+  width: 90vw;
+  max-width: 350px;
+  margin-left: 20px;
 
-`
+  .superDiv {
+    h2 {
+      font-weight: 400;
+    }
+    display: flex;
+    justify-content: center;
+    width: 88vw;
+    max-width: 350px;
+    border-radius: 4px 4px 0px 0px;
+    background-color: var(--primary-1);
+    color: var(--white);
+  }
+  button {
+    width: 33vw;
+    max-width: 150px;
+    height: 37px;
+    font-size: 10px;
+    border-radius: 0px 0px 4px 0px;
+    border-style: none;
+    padding: 10px;
+    background-color: var(--primary-1);
+    color: var(--white);
+    cursor: pointer;
+  }
+  button:hover {
+    background-color: var(--primary-2);
+  }
+  input {
+    border-radius: 0px 0px 0px 4px;
+    border-style: none;
+    background-color: #c4c4c4;
+    height: 37px;
+    width: 55vw;
+    max-width: 200px;
+    padding-left: 10px;
+    border: 0;
+    border-left: 4px;
+    border-bottom: 4px;
+    border-style: solid;
+    border-color: var(--primary-1);
+  }
+`;
 const DivChat = styled.div`
-width: 200px;
-height: 200px;
-border-color: 2px solid black;
-`
+  width: 88vw;
+  max-width: 350px;
+  height: 200px;
+  border: 0;
+  padding: 10px;
+  border-left: 4px;
+  border-right: 4px;
+  border-style: solid;
+  border-color: var(--primary-1);
+`;
+const DivInput = styled.div`
+  display: flex;
+  align-items: center;
+`;
 
-export {DivWrapper,DivChat}
+export { DivWrapper, DivInput, DivChat };
