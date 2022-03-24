1caca672bf4cab59b756a6f3c7b0449871371671
assemble05 <joneto18.jh@gmail.com>
2022-03-24 18:11:28 -0300

    feature chat

---
+++ src/components/ChatUser/style.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const DivWrapper = styled.div`
+   
+
+
+
+   div{
+       button{
+           border-radius: 4px 4px 4px 4px;
+
+       }
+   }
+`
+
+export {DivWrapper}
