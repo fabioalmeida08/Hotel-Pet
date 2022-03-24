c412f01a914f2154cce03cce03f25096aafc0b07
igorcenzi <igor.cenzi2@gmail.com>
2022-03-23 21:13:40 -0300

    fix: fixed

--- src/components/CardPets/StyledCardPet.tsx
+++ src/components/CardPets/StyledCardPet.tsx
@@ -1,5 +1,4 @@
-import styled from 'styled-components'
-
+import styled from "styled-components";
 export const style = {
   position: 'absolute',
   top: '50%',
@@ -11,61 +10,30 @@ export const style = {
   p: 4,
 }
 
-// export const StyledCardPet = styled.div`
-//   width: 350px;
-//   height: 230px;
-//   display: flex;
-//   align-items: flex-start;
-//   border-radius: 20px;
-//   background-color: #FFFFFF;
-//   color: #333333;
-//   position: relative;
-//   cursor: pointer;
-//   box-shadow: 9px 4px 15px 0px rgba(0,0,0,0.3);
-
-//   .div-button{
-//     display: flex;
-//     justify-content: space-between;
-//     align-items: center;
-//     gap: 4px;
-//   }
-
-// `
-
 export const StyledCardPet = styled.div`
+  width: 350px;
+  height: 230px;
   display: flex;
-  flex-direction: column;
-  box-shadow: 9px 4px 15px 0px rgba(0, 0, 0, 0.3);
-  padding: 10px;
+  align-items: flex-start;
+  border-radius: 20px;
+  background-color: #FFFFFF;
+  color: #333333;
   position: relative;
-
-  .paw {
+  cursor: pointer;
+  box-shadow: 9px 4px 15px 0px rgba(0,0,0,0.3);
+  .div-button{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    gap: 4px;
+  }
+  .paw{
     position: absolute;
     width: 200px;
     bottom: 10%;
     right: 5%;
     z-index: 0;
   }
-  @media screen and (min-width: 400px) {
-    width: 90%;
-    max-width: 350px;
-    height: 200px;
-    flex-direction: row;
-    align-items: flex-start;
-    border-radius: 20px;
-    background-color: #ffffff;
-    color: #333333;
-    position: relative;
-    box-shadow: 9px 4px 15px 0px rgba(0, 0, 0, 0.3);
-  }
-
-  @media screen and (min-width: 500px) {
-    display: block;
-    bottom: 4px;
-    right: 4px;
-    z-index: 0;
-  }
-
   .FotoPet {
     height: 100%;
     width: 50%;
@@ -82,16 +50,13 @@ export const StyledCardPet = styled.div`
       object-fit: cover;
     }
   }
-
-  .CardContainer {
+  .CardContainer{
     width: 100%;
     padding: 12px;
-
     flex: 2;
     //position: relative;
     z-index: 1;
   }
-
   .CardHeader {
     display: flex;
     justify-content: space-between;
@@ -99,60 +64,53 @@ export const StyledCardPet = styled.div`
     margin-bottom: 8px;
     h2 {
       font-size: 18px;
-      font-family: 'Roboto';
+      font-family: "Roboto";
     }
   }
-
   section div {
     display: flex;
     justify-content: space-between;
     align-items: center;
     font-size: 14px;
     margin-bottom: 4px;
-
     p {
-      font-family: 'Roboto';
+      font-family: "Roboto";
       font-weight: 500;
     }
-
     span {
-      color: '#606060';
+      color: "#606060";
       font-weight: 500;
     }
   }
-
-  button {
-    padding: 8px 16px;
-    border: none;
-    border-radius: 4px;
-    background: linear-gradient(
-      180deg,
-      rgba(244, 169, 25, 0.52) 0%,
-      #f4a919 100%
-    );
-    color: #ffffff;
-    font-size: 12px;
-    font-weight: 500;
-    font-family: 'Roboto';
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 4px;
-    margin-top: 8px;
-    cursor: pointer;
-
+button{
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  background: linear-gradient(180deg, rgba(244, 169, 25, 0.52) 0%, #F4A919 100%);
+  color: #FFFFFF;
+  font-size: 12px;
+  font-weight: 500;
+  font-family: 'Roboto';
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 4px;
+  margin-top: 8px;
+  cursor: pointer;
     :hover {
       background-color: var(--primary-1);
     }
-
-    img {
-      width: 16px;
-    }
-
-    .status {
-      cursor: pointer;
-    }
+  img{
+    width: 16px;
   }
+  .status {
+    cursor: pointer;
+  }
+}
+
+@media (min-width: 400px){
+  
+}
 `
 
 export const ButtonDiv = styled.div`
